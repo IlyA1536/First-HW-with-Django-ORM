9dab97027da4a92fb76ec0f5ba708a9098826a16
IlyA1536 <i0977187143@gmailcom>
2024-02-03 00:48:12 +0200

    Made a database with 3 tabels: genre, game, game_genre

---
+++ game_collection/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Genre (models.Model):
+    genre = models.CharField(max_length=100)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.ManyToManyField("Genre")
+    date_of_issue = models.DateField()
+    rating = models.DecimalField(decimal_places=1, max_digits=2)
